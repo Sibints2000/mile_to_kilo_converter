7b49462fcf1d59fcbfe9eeb38de3242bdc08fca8
Sibints2000 <sibints2000@gmail.com>
2021-08-22 22:31:32 +0530

    Third Commit

--- mile_to_kilo_converter.py
+++ mile_to_kilo_converter.py
@@ -3,8 +3,13 @@ from tkinter import *
 window = Tk()
 window.title("Miles To Kilometer Converter")
 
+mile_input = Entry()
 
+miles_label = Label(text="Miles")
 
+is_equal_label = Label(text="is equal to")
+
+kilometer_result_label = Label(text="0")
 
 
 window.mainloop()
