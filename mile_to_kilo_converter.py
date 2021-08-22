6455f02212552b014c5d41dcfdd2cec273baabde
Sibints2000 <sibints2000@gmail.com>
2021-08-22 22:49:59 +0530

    Twenty First Commit

--- mile_to_kilo_converter.py
+++ mile_to_kilo_converter.py
@@ -11,8 +11,8 @@ window = Tk()
 window.title("Miles To Kilometer Converter")
 window.config(padx=20, pady=20)
 
-mile_input = Entry(width=7)
-mile_input.grid(column=1, row=0)
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
 
 miles_label = Label(text="Miles")
 miles_label.grid(column=2, row=0)
